40f249af6881ed8652fb349e9b2a623f2e4b4769
Kerim Satirli <k.satirli@gmail.com>
2021-08-11 15:35:35 +0200

    adds list for HashiConf Digital EU 2021 speakers

---
+++ twitter/hashiconf.tf
@@ -0,0 +1,32 @@
+resource "twitter_list" "hashiconf_digital_europe_2021" {
+  name = "HashiConf Digital (Europe 2021)"
+  mode = "public"
+
+  description = "@HashiConf Digital (Europe 2021) Speakers"
+
+  members = [
+    "austingebauer",
+    "BeenzSyed",
+    "blakecova",
+    "build1point0",
+    "devarshishah3",
+    "devops_rob",
+    "foobly",
+    "hgmnz",
+    "jefferai",
+    "jimlambrt",
+    "jrasell",
+    "kevholditch",
+    "kmruddy",
+    "LucyDavinhart",
+    "mildwonkey",
+    "MordorBernd",
+    "pianotpot",
+    "ppacent",
+    "roary_tubbs",
+    "rustbeltrae",
+    "sarki247",
+    "sheriffjackson",
+    "tracey_pooh"
+  ]
+}
