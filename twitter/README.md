98f7ca04d59cf78824b73572f5d516db56908354
Kerim Satirli <k.satirli@gmail.com>
2021-08-11 14:12:07 +0200

    adds HashiTalks: Build Twitter List

---
+++ twitter/README.md
@@ -0,0 +1,3 @@
+# Codified Twitter Lists
+
+For app information, see [here](https://developer.twitter.com/en/portal/apps/21647633/settings).
---
+++ twitter/hashitalks.tf
@@ -0,0 +1,35 @@
+resource "twitter_list" "hashitalks_build" {
+  name = "HashiTalks: Build (2021)"
+  mode = "public"
+
+  description = "A list of speakers and contributors of HashiTalks: Build (2021)."
+
+  members = [
+    "amrtns",
+    "ASomesan",
+    "bflad",
+    "devopsjacquie",
+    "ericsmalling",
+    "evanphx",
+    "foobly",
+    "HansHasselberg",
+    "JColeMorrison",
+    "joatmon08",
+    "JudithPatudith",
+    "ksatirli",
+    "kmruddy",
+    "marycutrali",
+    "mildwonkey",
+    "onlydole",
+    "paddycarver",
+    "phillebaba",
+    "pst418",
+    "schristoff25",
+    "sweetpavement",
+    "ThatMightBePaul",
+    "tombuildsstuff",
+    "tr0njavolta",
+    "vlazarenko",
+    "YellsOld",
+  ]
+}
