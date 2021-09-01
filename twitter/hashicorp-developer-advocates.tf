7b6549770587960b3cd8272198318ce7a1f7c358
Kerim Satirli <k.satirli@gmail.com>
2021-09-01 16:37:02 +0200

    updates DA list title and adds `@justinklztr`

--- twitter/hashicorp-developer-advocates.tf
+++ twitter/hashicorp-developer-advocates.tf
@@ -1,27 +1,28 @@
 resource "twitter_list" "hashicorp_das" {
-  name = "HashiCorp DAs"
+  name = "HashiCorp DAs and TCMs"
   mode = "public"
 
   description = "(current and former) developer advocates and community folks at @HashiCorp."
 
   members = [
-    "JColeMorrison",
-    "SoFetchPhoto",
-    "SolutionGeek",
-    "XanderGrzy",
     "build1point0",
     "christi3k",
     "devops_rob",
     "devopsjacquie",
     "devopsjono",
     "erikveld",
+    "JColeMorrison",
     "joatmon08",
+    "justinklztr",
     "katiereese317",
     "ksatirli",
     "nicolerenee3810",
     "onlydole",
     "sethvargo",
     "sheriffjackson",
-    "tracypholmes"
+    "SoFetchPhoto",
+    "SolutionGeek",
+    "tracypholmes",
+    "XanderGrzy",
   ]
 }
